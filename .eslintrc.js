e95d31e05607fe03499208cd2fa0fb4858cad395
15915940450 <15915940450@139.com>
2017-12-26 10:45:28 +0800

    n, vue

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "jquery":true,
+    "commonjs":true,
+    "node": true,
+    "es6": true
+  },
+  "globals":{
+  },
+  parserOptions:{
+    sourceType: 'module'
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": ["error", 2],
+    "semi": ["error", "always"],
+    "comma-dangle": ["error", "never"],
+    "eqeqeq": ["error", "always"],
+    "quotes": ["error", "single"]
+  }
+};
